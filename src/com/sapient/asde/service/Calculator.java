06f390ab5f94c79d43632a3047612cc45051d717
Annapureddy Srinivas Reddy <digitech1993@gmail.com>
2022-06-06 21:09:59 +0530

    updated div() method

--- src/com/sapient/asde/service/Calculator.java
+++ src/com/sapient/asde/service/Calculator.java
@@ -14,6 +14,14 @@ public class Calculator {
     }
 
     public double div(double number1,double number2){
-        return number1/number2;
+        try {
+            if(number2==0.0){
+                throw new Exception("denominator cannot be zero")
+            }
+            return number1 / number2;
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return 0.0;
     }
 }
